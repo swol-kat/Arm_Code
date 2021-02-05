639fb2e27798043de5c9982c694c30eb78e92fb0
Arjun Gandhi <arjungandhi06@gmail.com>
2021-02-04 21:22:33 -0500

    refactor code electric boogaloo

--- src/InitOdrive.py
+++ src/InitOdrive.py
@@ -11,22 +11,22 @@ odrives = odrive.find_any(find_multiple=2)
 # Multiple Odrive setup:
 # Serial number for arm 1 lower and upper: 35593308029517
 # Serial number for arm 1 shoulder: 35623373914701
-axisDict = json.loads(open('axis_config.json', "r").read())
+axis_dict = json.loads(open('axis_config.json', "r").read())
 
 output_dict = {}
 for odrive in odrives:
-    c = axisDict[str(odrive.serial_number)]
+    c = axis_dict[str(odrive.serial_number)]
     output_dict[c['axis1']['name']] = Joint(odrive.axis1, c['axis1']['ratio'])
     output_dict[c['axis0']['name']] = Joint(odrive.axis0, c['axis0']['ratio'])
 
-armVariables = {'A1': 3.319, 'D1': 3.125, 'A2': 7.913, 'A3': 9.0}
+arm_variables = {'A1': 3.319, 'D1': 3.125, 'A2': 7.913, 'A3': 9.0}
 
-singleArm = Arm(output_dict['1 lower'], output_dict['1 upper'], output_dict['1 shoulder'], armVariables)
+single_arm = Arm(output_dict['1 lower'], output_dict['1 upper'], output_dict['1 shoulder'], arm_variables)
 
-singleArm.calibrate_arm()
+single_arm.calibrate_arm()
 
-singleArm.home_arm()
+single_arm.home_arm()
 
-singleArm.enable_arm()
+single_arm.enable_arm()
 
 print("Setup Complete!")
