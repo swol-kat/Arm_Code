58619268802c7dcf1cb4586a0003bc96f01f2126
Arjun Gandhi <arjungandhi06@gmail.com>
2021-02-04 22:24:58 -0500

    gyro boogoloo

--- src/gyro.py
+++ src/gyro.py
@@ -6,6 +6,4 @@ i2c = I2C(SCL, SDA)
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 
 while True:
-    print(f'temp: {sensor.temperature}')
-    print(f'euler: {sensor.euler}')
-    print(f'gravity: {sensor.gravity}')
+    print(f'temp: {sensor.temperature}, euler: {sensor.euler}, gravity: {sensor.gravity}')
