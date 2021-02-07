ab15e4de93db4b9eb60b95c776282435d067f1d4
Arjun Gandhi <arjungandhi06@gmail.com>
2021-02-07 12:59:58 -0500

    working flask server

--- src/arm_server.py
+++ src/arm_server.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+CORS(app)
 socketio = SocketIO(app)
 
+@app.route('/')
+def load_dashboard():
+    return "Fuck"
+
+
 if __name__ == '__main__':
-    socketio.run(app)
+    print('[INFO] Starting server at http://localhost:6969')
+    socketio.run(app=app, host='0.0.0.0', port=6969)
