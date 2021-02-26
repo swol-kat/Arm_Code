208fbaf2cc151a45038fcfdffb287c6dce4620d6
Arjun Gandhi <arjungandhi06@gmail.com>
2021-02-26 10:05:22 -0500

    removed print

--- src/robot/gaits/wiggle.py
+++ src/robot/gaits/wiggle.py
@@ -31,5 +31,4 @@ class Wiggle(Gait):
             target_foot_pos = np.array([zero_body_pts[i][0], zero_body_pts[i][1], 0])
             target_foot_pos = target_foot_pos - body_pts[i]
             target_foot_pos = get_rot_leg_orig(i).transpose() @ rot.transpose() @ target_foot_pos
-            print(target_foot_pos)
             leg.target_pos = target_foot_pos
