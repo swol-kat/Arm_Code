b45b4d0f09473ff13261d26f78c3008959a1490b
Arjun Gandhi <arjungandhi06@gmail.com>
2021-03-30 18:37:06 -0400

    not sure

--- src/robot/gaits/openwalk.py
+++ src/robot/gaits/openwalk.py
@@ -18,7 +18,7 @@ class OpenWalk(Gait):
         self.last_loop_time = time.time()
         self.last_time = time.time()
         self.state = [1, 3]
-        self.movement_vector = {}
+        self.movement_vector = {'x': 0, 'y': 0,'z':0, 'alpha':0,'beta':0,'gamma':0}
         self.x_vel = 0
         self.y_vel = 0
         self.count = 0
@@ -33,22 +33,25 @@ class OpenWalk(Gait):
 
 
         body_pts = get_body_pts(robot.target_base_state, width, length, False)
-        dt = time.time()-self.last_time
+        
+        pos_adjust = {k: v*(time.time()-self.last_time) for k,v in self.movement_vector.items()}
         self.last_time=time.time()
-        pos_adjust = {k: v*dt*.5 for k,v in self.movement_vector.items()}
         robot.target_base_state.move(**pos_adjust)
+
+
         rot = euler_tm(robot.target_base_state.alpha, robot.target_base_state.beta, robot.target_base_state.gamma)
+        
         l = math.hypot(self.x_vel, self.y_vel) * step_time
-        phi = math.atan2(self.y_vel, self.x_vel)
+        phi = math.atan2(self.y_vel, self.x_vel) 
         d_swing = swing_pos(min(delta_t / step_time, 1), self.params['step_height'], l, phi)
         d_ground = ground_pos(min(delta_t / step_time, 1), l, phi)
+
         for i, leg in enumerate(robot.arms):
             target_foot_pos = np.array([body_pts[i][0], body_pts[i][1], 0]) + self.prev_foot_pos[i]
             if i in self.state:
                 target_foot_pos += d_swing
             else:
                 target_foot_pos += d_ground
-
             target_foot_pos = target_foot_pos - body_pts[i]
             if delta_t > step_time:
                 self.prev_foot_pos[i] = np.append(target_foot_pos[:2],0)
@@ -62,8 +65,8 @@ class OpenWalk(Gait):
             self.next_state()
             if self.count == 1:
                 self.movement_vector = robot.movement_vector
-                self.x_vel = robot.movement_vector['x']
-                self.y_vel = robot.movement_vector['y']
+                self.x_vel = robot.movement_vector['x'] * 2
+                self.y_vel = robot.movement_vector['y'] * 2
 
             self.count = (self.count + 1) % 2
 
